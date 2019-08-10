e01daf52f145b9d71baecd0cb6648e69c392d056
jokmenen <Jokmenen@gmail.com>
2019-08-10 17:35:05 +0200

    Added timer to HTML, added functionality via Timer.js.

--- app.py
+++ app.py
@@ -6,7 +6,9 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return render_template('index.html')
+    timer_data = "Jan 5, 2021 15:37:25"
+    timer_name = "TIMER 1"
+    return render_template('index.html', timer_name = timer_name, timer_data = timer_data)
 
 if __name__ == '__main__':
     app.run()
